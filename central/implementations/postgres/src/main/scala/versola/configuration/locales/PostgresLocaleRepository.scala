665c8ad2201c922d1ed0212c08a5aee6faf75041
Bekezhan <arginb6@gmail.com>
2026-08-08 16:12:53 +0500

    fix: locale repository isolation level #97

--- central/implementations/postgres/src/main/scala/versola/configuration/locales/PostgresLocaleRepository.scala
+++ central/implementations/postgres/src/main/scala/versola/configuration/locales/PostgresLocaleRepository.scala
@@ -15,7 +15,7 @@ class PostgresLocaleRepository(xa: TransactorZIO) extends LocaleRepository, Basi
       sql"""SELECT code, name, is_default, active FROM locales ORDER BY code""".query[LocaleRecord].run()
 
   override def update(add: Vector[LocaleRecord], delete: Vector[String]): Task[Unit] =
-    xa.repeatableRead.transactMeasured("update-locales"):
+    xa.transactMeasured("update-locales"):
       if delete.nonEmpty then
         sql"""DELETE FROM locales WHERE code = ANY($delete)""".update.run()
         sql"""UPDATE forms
@@ -30,9 +30,8 @@ class PostgresLocaleRepository(xa: TransactorZIO) extends LocaleRepository, Basi
       }
 
   override def setDefault(code: String): Task[Unit] =
-    xa.repeatableRead.transactMeasured("set-default-locale"):
-      sql"""UPDATE locales SET is_default = FALSE WHERE is_default = TRUE""".update.run()
-      sql"""UPDATE locales SET is_default = TRUE WHERE code = $code""".update.run()
+    xa.transactMeasured("set-default-locale"):
+      sql"""UPDATE locales SET is_default = (code = $code)""".update.run()
     .unit
 
 object PostgresLocaleRepository:
